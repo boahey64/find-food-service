2d0da2c772189fadd20a19c9680d1532555d5c5d
roman <roman.richter.git.user>
2020-01-21 21:54:27 +0100

    Added unit test for controller to test service call against mock service

---
+++ src/test/java/com/example/findfood/controller/SearchFoodControllerTest.java
@@ -0,0 +1,62 @@
+package com.example.findfood.controller;
+
+import com.example.findfood.service.SearchFoodService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fatsecret.platform.model.CompactFood;
+import com.fatsecret.platform.services.Response;
+import com.google.gson.Gson;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@RunWith(JUnitPlatform.class)
+class SearchFoodControllerTest {
+    @InjectMocks
+    SearchFoodController searchFoodController;
+
+    @Mock
+    SearchFoodService searchFoodService;
+
+    private String path = "/api/foods";
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @Test
+    public void call_search() throws Exception {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+
+        when(searchFoodService.searchFoodItems()).thenReturn(aResponseOfCompactFoodItems());
+
+        Response<CompactFood> actual = searchFoodController.showSearch();
+
+        assertThat(actual, notNullValue());
+        assertEquals(actual.getPageNumber(), 3);
+    }
+
+
+    private Response<CompactFood> aResponseOfCompactFoodItems() {
+        Gson g = new Gson();
+        String jsonString = "{\"pageNumber\":3,\"maxResults\":50,\"totalResults\":2000,\"results\":[]}";
+        return g.fromJson(jsonString, Response.class);
+    }
+}
